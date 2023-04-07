85c971e2fac712b5acca57a2043006cbe457a9fb
Spiff Jekey-Green <spiffjekeygreen@gmail.com>
2023-04-07 15:02:54 -0700

    Update constant.php

--- syste/main/constant.php
+++ syste/main/constant.php
@@ -2,7 +2,7 @@
  
 //normal development configuration
 $mode = "development";
-$base_url = "http://blopz.com/";
+$base_url = "https://blopz.com/";
 // $base_url = "http://blopz-env-1.eba-kc9taepc.us-east-1.elasticbeanstalk.com/";
 $processor_url = "syste/main/system.php";
 
